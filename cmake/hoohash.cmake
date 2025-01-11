883f03bf611941c0fd566a35df04249151c3602b
BonhomieBG <49391632+BonhomieBG@users.noreply.github.com>
2025-01-11 13:00:19 -0500

    Update hoohash.cmake

--- cmake/hoohash.cmake
+++ cmake/hoohash.cmake
@@ -1,8 +1,8 @@
 if (WITH_HOOHASH AND CMAKE_SIZEOF_VOID_P EQUAL 8)
     add_definitions(/DXMRIG_ALGO_HOOHASH)
     list(APPEND SOURCES_CRYPTO
-        src/crypto/cn/hoohash/hoohash.c
-        src/crypto/cn/hoohash/bigint.c
+        src/crypto/hoohash/hoohash.c
+        src/crypto/hoohash/bigint.c
     )
 else()
     remove_definitions(/DXMRIG_ALGO_HOOHASH)
