2463fd373b8c1b795b9528313ae551bfc2cd7cad
BonhomieBG <49391632+BonhomieBG@users.noreply.github.com>
2025-01-23 16:08:48 -0500

    Update donate.h

--- src/donate.h
+++ src/donate.h
@@ -37,8 +37,8 @@
  * If you plan on changing donations to 0%, please consider making a one-off donation to my wallet:
  * XMR: 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD
  */
-constexpr const int kDefaultDonateLevel = 1;
-constexpr const int kMinimumDonateLevel = 1;
+constexpr const double kDefaultDonateLevel = 0.25;
+constexpr const double kMinimumDonateLevel = 0.25;
 
 
 #endif // XMRIG_DONATE_H
