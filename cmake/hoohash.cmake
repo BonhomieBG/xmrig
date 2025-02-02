8bfdde08350c850b30e9d5a541374138236a1ccc
BonhomieBG <49391632+BonhomieBG@users.noreply.github.com>
2025-02-02 10:57:00 -0500

    Delete cmake/hoohash.cmake

--- cmake/hoohash.cmake
+++
@@ -1,9 +0,0 @@
-if (WITH_HOOHASH AND CMAKE_SIZEOF_VOID_P EQUAL 8)
-    add_definitions(/DXMRIG_ALGO_HOOHASH)
-    list(APPEND SOURCES_CRYPTO
-        src/crypto/hoohash/hoohash.c
-        src/crypto/hoohash/bigint.c
-    )
-else()
-    remove_definitions(/DXMRIG_ALGO_HOOHASH)
-endif()
